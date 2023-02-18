3a928e6a8bedcd737249cd575f73440b5b2080c5
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-02-18 14:38:16 -0500

    Getters and updaters in Manim

---
+++ manim_tut6.py
@@ -0,0 +1,18 @@
+from manim import *
+
+class Getters(Scene):
+    def construct(self):
+
+        rect = Rectangle(color=WHITE, height=3, width=2.5).to_edge(UL)
+        circ = Circle().to_edge(DOWN)
+        arrow = always_redraw(
+            lambda: Line(
+                    start=rect.get_bottom(), end=circ.get_top(), buff=0.2
+                ).add_tip()
+        )  # buff to not make it touch circle
+        # always_redraw so that it moves along with the rectangle
+
+        self.play(Create(VGroup(rect, circ, arrow)))
+        self.wait()
+
+        self.play(rect.animate.to_edge(UR), circ.animate.scale(0.5), run_time=4)
