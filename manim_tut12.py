051f678d6eee7524bc041ffb24048c8bf36edfce
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-02-19 16:43:39 -0500

    added comments to tut12 and syncing with remote

--- manim_tut12.py
+++ manim_tut12.py
@@ -5,10 +5,11 @@ HOME = "C:\\Users\\anura\\Documents\\VSCode_Workspace\\Learning_Manim"
 class SVGs(Scene):
     def construct(self):
 
+        # Adding SVGs and Regular images
         icon = SVGMobject(f"{HOME}\\6god2.svg").scale(3)
         im = ImageMobject(f"{HOME}\\owl.png").scale(0.95).to_edge(UR)
         im2 = ImageMobject(f"{HOME}\\ovo.png").scale(0.3).to_edge(UL)
 
         self.play(DrawBorderThenFill(icon)) #only works on vectorized objects
-        self.play(FadeIn(im, im2))
+        self.play(FadeIn(im, im2)) # to render PNGs, JPEGs
         self.wait()
